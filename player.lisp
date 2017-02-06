9b7adba9fc6110db4426a6141f71fa5fde7e1ece
Eugene Zaikonnikov <eugene@funcall.org>
2017-02-06 22:48:59 +0100

    first visible frame

--- player.lisp
+++ player.lisp
@@ -6,10 +6,7 @@
        (with-slots (height width stream-records) avi
 	 (let* ((display (xlib:open-default-display))
 		(window (xlib:create-window :parent (xlib:screen-root (xlib:display-default-screen display))
-					    :x 0
-					    :y 0
-					    :width width
-					    :height height
+					    :x 0 :y 0 :width width :height height
 					    :event-mask '(:exposure :key-press)))
 		(gc (xlib:create-gcontext :drawable window))
 		(pixmap (xlib:create-pixmap :width width :height height :depth 24 :drawable window))
@@ -23,19 +20,19 @@
 	     (xlib:map-window window)
 	     (xlib:event-case (display :discard-p t)
 	       (exposure ()
-			      (loop for i from 0 below height
-				 with src = (frame (pop (rcursor rec))) do
-				   (loop for j from 0 below width
-				      for spos = (* 3 (+ j (* width i))) do
-					(setf (aref buffer j i)
-					      (logand (ash (aref src (+ 2 spos)) 16) (ash (aref src (1+ spos)) 8) (aref src spos)))))
-			      (sleep (/ (scale rec) (rate rec)))
-			      (xlib:put-image pixmap pixmap-gc image :width width :height height :x 0 :y 0)
-			      (xlib:copy-area pixmap gc 0 0 width height window 0 0)
-			      (xlib:display-force-output display)
-			      nil)
+			 (loop for i from 0 below height
+			    with src = (frame (pop (rcursor rec))) do
+			      (loop for j from 0 below width
+				 for spos = (* 3 (+ j (* width i))) do
+				   (setf (aref buffer j i)
+					 (logior (ash (aref src (+ 2 spos)) 16) (ash (aref src (1+ spos)) 8) (aref src spos)))))
+			 (sleep (/ (scale rec) (rate rec)))
+			 (xlib:put-image pixmap pixmap-gc image :width width :height height :x 0 :y 0)
+			 (xlib:copy-area pixmap gc 0 0 width height window 0 0)
+			 (xlib:display-force-output display)
+			 nil)
 	       (key-press ()
-			       t)))
+			  t)))
 	   (xlib:free-pixmap pixmap)
 	   (xlib:free-gcontext gc)
 	   (xlib:close-display display))))))
