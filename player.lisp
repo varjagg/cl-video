47a18d2a9feacef400d36870f96aff15b2519489
Eugene Zaikonnikov <eugene@funcall.org>
2017-02-06 16:11:51 +0100

    clx player

--- player.lisp
+++ player.lisp
@@ -1,5 +1,5 @@
 (in-package :cl-video)
-
+#|
 (use-package :kit.sdl2)
 
 (defclass player-window (window)
@@ -38,3 +38,51 @@
 (defun play ()
   (sdl2.kit:init)
   (make-instance 'player-window))
+
+|#
+
+(use-package :xlib)
+ 
+(defmethod play-stream ((avi avi-mjpeg-stream))
+  (bt:make-thread 
+   #'(lambda ()
+       (with-slots (height width stream-records) avi
+	 (let* ((display (open-default-display))
+		(window (create-window :parent (screen-root (display-default-screen display))
+				       :x 0
+				       :y 0
+				       :width width
+				       :height height
+				       :event-mask '(:exposure :key-press)))
+		(gc (create-gcontext :drawable window))
+		(pixmap (create-pixmap :width width :height height :depth 24 :drawable window))
+		(pixmap-gc (create-gcontext :drawable pixmap))
+		(buffer (make-array `(,width ,height) :element-type 'xlib:pixel))
+		(image (create-image  :data buffer :depth 24
+				      :height height :width width))
+		(rec (find-if #'(lambda (x) (eql (type-of x) 'mjpeg-stream-record)) stream-records)))
+	   (unwind-protect
+		(setf (wm-name window) (pathname-name (filename avi)))
+	     (map-window window)
+	     (event-case (display :discard-p t)
+	       (exposure ()
+			 (loop for i from 0 below height
+			    with src = (frame (pop (rcursor rec))) do
+			      (loop for j from 0 below width
+				 for spos = (* 3 (+ j (* width i))) do
+				      (setf (aref buffer j i)
+					    (logand (ash (aref src (+ 2 spos)) 16) (ash (aref src (1+ spos)) 8) (aref src spos)))))
+			 (sleep (/ (scale rec) (rate rec)))
+			 (put-image pixmap pixmap-gc image :width width :height height :x 0 :y 0)
+			 (copy-area pixmap gc 0 0 width height window 0 0)
+			 (display-force-output display)
+			 nil #| continue receiving events |#)
+	       (key-press ()
+			  t #| non-nil result signals event-case to exit |#)))
+	 (free-pixmap pixmap)
+	 (free-gcontext gc)
+	 (close-display display))))))
+
+(defun play (pathname)
+  (decode-file pathname :player-callback #'play-stream))
+
