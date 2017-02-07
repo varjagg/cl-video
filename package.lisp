c437673ef41dc2b4d7cfd4b836b03963349d092a
Eugene Zaikonnikov <eugene@funcall.org>
2017-02-07 17:20:38 +0100

    exports

--- package.lisp
+++ package.lisp
@@ -2,5 +2,9 @@
 
 (defpackage #:cl-video
   (:use #:cl)
-  (:export #:decode-file #:decode))
+  (:export #:decode-file #:decode #:find-mjpeg-stream-record #:avi-mjpeg-stream
+	   #:mjpeg-stream-record #:audio-stream-record
+	   #:stream-playback-start #:stream-playback-stop #:chunk
+	   #:media-decoder-error #:unrecognized-file-format #:unsupported-avi-file-format #:malformed-avi-file-format
+	   #:rcursor #:vacancy-lock #:scale #:rate #:filename #:final))
 
