45f35be5d510ea5a96cad0f0749a93ab45801122
Eugene Zaikonnikov <eugene@funcall.org>
2017-03-28 16:32:13 +0200

    exports

--- package.lisp
+++ package.lisp
@@ -6,7 +6,7 @@
 	   #:mjpeg-stream-record #:audio-stream-record #:initialize-ring #:decode-media-stream #:video-stream-record
 	   #:stream-playback-start #:stream-playback-stop #:chunk #:prime-all-streams #:gif-container
 	   #:media-decoder-error #:unrecognized-file-format #:unsupported-avi-file-format #:malformed-avi-file-format
-	   #:wcursor #:rcursor #:vacancy-lock #:scale #:rate #:filename #:final #:finish #:frame-delay
+	   #:wcursor #:rcursor #:vacancy-lock #:scale #:rate #:filename #:final #:finish #:frame-delay #:frame-size
 	   #:frame #:pause-lock #:vacancy-lock #:pause #:height #:width #:start #:suggested-buffer-size
 	   #:number-of-channels #:block-align #:buffer #:compression-code #:stream-records #:significant-bits-per-sample
 	   #:+pcmi-uncompressed+ #:pop-chunk-rcursor #:audio-out #:audio-rec #:initialize-sink #:close-sink #:sink-frame
