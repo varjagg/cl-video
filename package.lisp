3a88a10df1b8f28d887f9e6290f4575555e7827c
Eugene Zaikonnikov <eugene@funcall.org>
2017-03-28 09:34:56 +0200

    exports

--- package.lisp
+++ package.lisp
@@ -2,12 +2,13 @@
 
 (defpackage #:cl-video
   (:use #:cl #:alexandria)
-  (:export #:decode-file #:decode #:find-video-stream-record #:find-pcm-stream-record #:av-container #:avi-mjpeg-container
+  (:export #:decode-file #:decode #:find-video-stream-record #:av-container #:avi-mjpeg-container
 	   #:mjpeg-stream-record #:audio-stream-record #:initialize-ring #:decode-media-stream #:video-stream-record
 	   #:stream-playback-start #:stream-playback-stop #:chunk #:prime-all-streams #:gif-container
 	   #:media-decoder-error #:unrecognized-file-format #:unsupported-avi-file-format #:malformed-avi-file-format
 	   #:wcursor #:rcursor #:vacancy-lock #:scale #:rate #:filename #:final #:finish #:frame-delay
 	   #:frame #:pause-lock #:vacancy-lock #:pause #:height #:width #:start #:suggested-buffer-size
 	   #:number-of-channels #:block-align #:buffer #:compression-code #:stream-records #:significant-bits-per-sample
-	   #:+pcmi-uncompressed+ #:pop-chunk-rcursor))
+	   #:+pcmi-uncompressed+ #:pop-chunk-rcursor #:audio-out #:audio-rec #:initialize-sink #:close-sink #:sink-frame
+	   #:audio-output #:portaudio-pcm-output #:sink-frame-element-type #:translate-source-frame))
 
