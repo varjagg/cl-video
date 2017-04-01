bcd4677787fda85cbe58a8b241a3ccc5a484d866
Eugene Zaikonnikov <eugene@funcall.org>
2017-04-01 17:34:33 +0200

    export

--- package.lisp
+++ package.lisp
@@ -10,5 +10,6 @@
 	   #:frame #:pause-lock #:vacancy-lock #:pause #:height #:width #:start #:suggested-buffer-size
 	   #:number-of-channels #:block-align #:buffer #:compression-code #:stream-records #:significant-bits-per-sample
 	   #:+pcmi-uncompressed+ #:pop-chunk-rcursor #:audio-out #:audio-rec #:initialize-sink #:close-sink #:sink-frame
-	   #:audio-output #:portaudio-pcm-output #:sink-frame-element-type #:translate-source-frame #:wav-container))
+	   #:audio-output #:portaudio-pcm-output #:sink-frame-element-type #:translate-source-frame #:wav-container
+	   #:find-pcm-stream-record))
 
